f7bf7daabe62a01428ca205366190cd5469788d3
Gustavo Linhaus <gustavo_linhaus@hotmail.com>
2022-02-09 21:01:45 -0300

    refactor-exception

--- src/main/java/com/basis/turma/sgc/service/CompetenciaService.java
+++ src/main/java/com/basis/turma/sgc/service/CompetenciaService.java
@@ -4,7 +4,7 @@ import com.basis.turma.sgc.domain.Competencia;
 import com.basis.turma.sgc.repository.CompetenciaRepository;
 import com.basis.turma.sgc.service.dto.competencia.CompetenciaDTO;
 import com.basis.turma.sgc.service.dto.competencia.CompetenciaListaDTO;
-import com.basis.turma.sgc.service.exception.RegraNegocioException;
+import com.basis.turma.sgc.service.exception.regra.Exception;
 import com.basis.turma.sgc.service.mapper.competencia.CompetenciaListaMapper;
 import com.basis.turma.sgc.service.mapper.competencia.CompetenciaMapper;
 import lombok.RequiredArgsConstructor;
@@ -24,7 +24,7 @@ public class CompetenciaService {
 
     public CompetenciaListaDTO buscar(Integer id) {
         Competencia competencia = competenciaRepository.findById(id)
-                .orElseThrow(() -> new RegraNegocioException("Competência não encontrada!"));
+                .orElseThrow(() -> new Exception("Competência não encontrada!"));
 
         return competenciaListaMapper.paraDTO(competencia);
     }
@@ -50,8 +50,8 @@ public class CompetenciaService {
     public void excluir(Integer id) {
         try {
             competenciaRepository.deleteById(id);
-        } catch (Exception e) {
-            throw new RegraNegocioException("Não foi possível excluir a competência!");
+        } catch (java.lang.Exception e) {
+            throw new Exception("Não foi possível excluir a competência!");
         }
     }
 }
