e9d90d07b270734c4f432990dac5b98fccdd53ae
Leonardo Inácio <leonardoinaciosilva@hotmail.com>
2022-02-28 18:37:10 -0300

    Sem backend

--- backend/src/main/java/com/basis/turma/sgc/repository/SenioridadeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.basis.turma.sgc.repository;
-
-import com.basis.turma.sgc.domain.Senioridade;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SenioridadeRepository extends JpaRepository<Senioridade, Integer> {
-}
